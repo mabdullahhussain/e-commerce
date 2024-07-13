bba2946f2036db61b66a4c2ed915196e1c637393
mabdullahhussain <hussain15258284@gmail.com>
2024-07-14 01:04:19 +0500

    code added.

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -21,31 +21,9 @@ const Dashboard = ()=>{
     }
     return (
             <>
-                <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </div>
-                    </div>
-                    <a className="btn btn-ghost text-xl">E-Commerce Website</a>
-                </div>
-                <div className="text-center">
-                <h1 className="text-5xl  font-bold text-center text">DashBoard</h1>
-                </div>
-                </div>
-
+            <div className="text-center">
+            <h1 className="btn btn-ghost text-4xl font-bold ">DashBoard</h1>
+            </div>
                 <div  className="flex justify-center gap-6 flex-wrap mt-6">
                 {products.map(item => {
                     return <div  onClick={() => gotoDetail(item)}>
--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -24,37 +24,10 @@ const Detail = () => {
 
     return (
         <>
-            <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                </div>
-                <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                </ul>
-                </div>
-                <a className="btn btn-ghost text-xl">E-Commerce Website</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-            <h1 className=" mt-4 mb-4 text-center text-5xl font-bold">Product Detail</h1>
-            </div>
-            <div className="navbar-end card-actions justify-center">
-                <button onClick={goBack} className="btn btn-primary mb-4">Go Back</button>
-            </div>
-            </div>
+        <div className="text-center">    
+        <h1 className="btn btn-ghost text4xl font-bold ">Product Detail</h1>
+        <button onClick={goBack} className="btn btn-primary mb-4">Go Back</button>
+        </div>
                 <div  className="flex justify-center">
                 {product ? (
                     <div>
